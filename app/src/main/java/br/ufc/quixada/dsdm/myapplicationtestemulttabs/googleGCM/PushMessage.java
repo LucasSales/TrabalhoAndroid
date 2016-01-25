83af666df2861aec452ba0a7ad3697c59a39d59a
LucasSales <lucasalesvieira2@gmail.com>
2016-01-25 19:35:52 -0300

    builders

---
+++ app/src/main/java/br/ufc/quixada/dsdm/myapplicationtestemulttabs/googleGCM/PushMessage.java
@@ -0,0 +1,30 @@
+package br.ufc.quixada.dsdm.myapplicationtestemulttabs.googleGCM;
+
+/**
+ * Created by lucas on 25/01/16.
+ */
+public class PushMessage {
+    private String titulo;
+    private String mensagem;
+
+    public PushMessage(String titulo, String menssagem){
+        this.titulo = titulo;
+        this.mensagem = menssagem;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
