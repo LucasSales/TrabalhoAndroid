2d9519b8040fb3a7b4a10abf7a5af0fb8e6446e3
LucasSales <lucasalesvieira2@gmail.com>
2016-02-14 13:54:28 -0300

    ipmudado

--- app/src/main/java/br/ufc/quixada/dsdm/myapplicationtestemulttabs/constantes/Constantes.java
+++ app/src/main/java/br/ufc/quixada/dsdm/myapplicationtestemulttabs/constantes/Constantes.java
@@ -4,7 +4,7 @@ package br.ufc.quixada.dsdm.myapplicationtestemulttabs.constantes;
  * Created by lucas on 11/02/16.
  */
 public class Constantes {
-    public static final String IP_SERVIDOR ="192.168.1.4:80";
+    public static final String IP_SERVIDOR ="192.168.56.1:80";
     public static final String BROADCAST_NOME = "MENSAGENS";
     public static String NOME_USUARIO;
 }
